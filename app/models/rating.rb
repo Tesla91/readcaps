82e8a55bf3fcac5b4316ab294de9468ef5f56f49
Nicola Pilcher <nicola.pilcher@gmail.com>
2022-02-28 17:52:31 +0100

    added validations and associations

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,4 +1,7 @@
 class Rating < ApplicationRecord
   belongs_to :user
   belongs_to :recap
+
+  validates :star, presence: true
+  validates :comment, presence: true, length: { in: 10..200 }
 end
--- app/models/recap.rb
+++ app/models/recap.rb
@@ -1,4 +1,7 @@
 class Recap < ApplicationRecord
   belongs_to :user
   belongs_to :book
+
+  validates :summary, presence: true, length: { minimum: 50 }
+  validates :user, uniqueness: { scope: :book }
 end
