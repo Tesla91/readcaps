7b020c7c79664fe8c3ff50f4de8a91576f9222f6
Melissa Hauck <lissahauck@outlook.com>
2022-03-01 17:44:54 +0100

    api with seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require "open-uri"
+require "json"
+
+puts "starting seed"
+
+url = "https://www.googleapis.com/books/v1/users/102817767719508217692/bookshelves/0/volumes?key=AIzaSyB2JkrpLAnGFV2hqVXiJAYSdMwv6hqSFKA"
+user_serialized = URI.open(url).read
+user = JSON.parse(user_serialized)
+books = user["items"]
+
+books.each do |book|
+ Book.create(title: book["volumeInfo"]["title"], author: book["volumeInfo"]["authors"],
+description: book["volumeInfo"]["description"],
+photo_url: book["volumeInfo"]["imageLinks"]["thumbnail"])
+end
+
+puts "ending seed"
