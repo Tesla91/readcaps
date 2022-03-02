76875db7dccb58e71038717a5a80a4dc8893536e
Anna Eiden <annaverenaeiden@hotmail.de>
2022-03-02 11:40:49 +0100

    Added title to model recap

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_28_114952) do
+ActiveRecord::Schema.define(version: 2022_03_02_103805) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
