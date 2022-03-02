1b0f06f6c7e7ff88a24d7e952f6b65bc46f48a9e
Michelle Knolly <michelleknolly@gmail.com>
2022-03-02 11:52:01 +0100

    Added validation to rating

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -2,6 +2,6 @@ class Rating < ApplicationRecord
   belongs_to :user
   belongs_to :recap
 
-  validates :star, presence: true
+  validates :star, presence: true, inclusion: { in: 0..5 }, numericality: { only_integer: true }
   validates :comment, presence: true, length: { in: 10..200 }
 end
