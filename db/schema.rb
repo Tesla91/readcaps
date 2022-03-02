b09c28777e5b190103c7df1ff05f7fe8c238d758
Michelle Knolly <michelleknolly@gmail.com>
2022-03-02 11:44:20 +0100

    Added pgsearch as book search function

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_28_114952) do
+ActiveRecord::Schema.define(version: 2022_03_02_102839) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,15 @@ ActiveRecord::Schema.define(version: 2022_02_28_114952) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["recap_id"], name: "index_favorites_on_recap_id"
     t.index ["user_id"], name: "index_favorites_on_user_id"
+  end
+
+  create_table "pg_search_documents", force: :cascade do |t|
+    t.text "content"
+    t.string "searchable_type"
+    t.bigint "searchable_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["searchable_type", "searchable_id"], name: "index_pg_search_documents_on_searchable"
   end
 
   create_table "ratings", force: :cascade do |t|
