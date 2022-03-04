a24082085888349fbe787af4684ae2782362fcba
Michelle Knolly <michelleknolly@gmail.com>
2022-03-04 13:06:16 +0100

    Added recap cards to show page and fixed style

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -3,5 +3,4 @@ class Rating < ApplicationRecord
   belongs_to :recap
 
   validates :star, presence: true, inclusion: { in: 0..5 }, numericality: { only_integer: true }
-  validates :comment, presence: true, length: { in: 10..600 }
 end
