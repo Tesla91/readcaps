d4e8fcd8208cbe903badff93665ece6bda9025c1
Anna Eiden <annaverenaeiden@hotmail.de>
2022-03-09 13:03:17 +0100

    Added method to rank best rated user

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,6 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
 
   def home
+    @top_users = User.all.sort_by { |user| user.avg_received_ratings}.reverse
   end
 end
