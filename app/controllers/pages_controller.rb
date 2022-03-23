2df2ca62ce597ee265b33309fea7bdfad83a360e
Nicola Pilcher <nicola.pilcher@gmail.com>
2022-03-23 12:01:17 +0100

    refactored average user rating call

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: %i[home about]
 
   def home
-    @top_users = User.all.sort_by { |user| user.avg_received_ratings}.reverse
+    @top_users = User.all.sort_by(&:avg_received_ratings).reverse
   end
 
   def about; end
