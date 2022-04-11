875de4a94d9b8d1972f655d841c40e6fb2a92699
Nicola Pilcher <nicola.pilcher@gmail.com>
2022-04-11 15:35:39 +0200

    made call to db to get the top authors

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,7 @@ class PagesController < ApplicationController
 
   def home
     @top_users = User.all.sort_by(&:avg_received_ratings).reverse
+    @top_authors = Book.group(:author).count
   end
 
   def about; end
