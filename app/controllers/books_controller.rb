c77b589d35188e45341db748a7d641fd88066f0c
Nicola Pilcher <nicola.pilcher@gmail.com>
2022-04-18 15:37:34 +0200

    removed all of the ifs for the search bar since we changed to search recaps

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,11 +5,7 @@ class BooksController < ApplicationController
   skip_before_action :authenticate_user!, only: :index
 
   def index
-    if params[:query].present?
-      @books = Book.search_by_title_and_author(params[:query])
-    else
-      @books = Book.all
-    end
+    @books = Book.all
   end
 
   def new
